9adaddfaa6ab1bec00feff00bd3ff8ad47d8bdd8
odd:odd <eehd80yul@gmail.com>
2024-12-26 17:37:35 +0900

    컴포넌트

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = (props) => {
+  // props = {name: '확인'}
+  return (
+    <button className='bg-blue-500 text-white py-3 px-5'>{props.name}</button>
+  )
+}
+
+export const Button2 = (props) => {
+  // props = {children: '새로고침'}
+  return (
+    <button className='bg-blue-500 text-white py-3 px-5'>{props.children}</button>
+  )
+}
+
+export default Button
