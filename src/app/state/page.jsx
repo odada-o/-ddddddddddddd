949dad0658ae246e4b9e3c3e41a25aa338493eb1
odd:odd <eehd80yul@gmail.com>
2025-01-03 17:48:32 +0900

    react

--- src/app/state/page.jsx
+++ src/app/state/page.jsx
@@ -1,17 +1,49 @@
 "use client"
+import classNames from 'classnames'
+import { Backpack, House, Waves } from 'lucide-react'
 import React, { useState } from 'react'
+
+const navItems = [
+  { icon: <Waves />, text: '최고의 전망' },
+  { icon: <Backpack />, text: '컬처 아이콘' },
+  { icon: <House />, text: '한옥' }
+]
 
 const StatePage = () => {
   // const [변수, 변수를변경하는함수] = useState(초기값)
   const [number, setNumber] = useState(0)
   const [isOpen, setIsOpen] = useState(false)
+
+  const [selected, setSelected] = useState(0)
   return (
     <div>
+
+      <div>
+        <ul className='flex gap-3'>
+          {navItems.map((item, index) => (
+            <li key={index} className={classNames(
+              'flex flex-col items-center p-4 cursor-pointer',
+              index === selected ? 'bg-blue-500 text-white' : 'bg-white'
+            )}
+            onClick={() => {setSelected(index)}}
+            >
+              {item.icon} {item.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+
       <div>
         <button onClick={() => {setIsOpen(!isOpen)}} className='p-3 bg-blue-500 fixed top-0 right-0 z-10'>
           {isOpen ? 'x' : '='}
         </button>
-        <div className={`fixed top-0 w-full h-screen bg-gray-300 ${isOpen ? 'left-0' : 'left-full'}`}>전체 메뉴</div>
+        {/* <div className={`fixed top-0 w-full h-screen bg-gray-300 ${isOpen ? 'left-0' : 'left-full'}`}>전체 메뉴</div> */}
+
+        <div className={classNames(
+          'fixed top-0 w-full h-screen bg-gray-300',
+          isOpen ? 'left-0' : 'left-full'
+        )}>전체 메뉴</div>
 
 
 
